10e8af5493b3e69f5b9ae3de5633bb5832490c1c
SprintGrad <138903886+SprintGrad@users.noreply.github.com>
2023-09-08 19:43:31 +0530

    Add files via upload

---
+++ introduction_.py
@@ -0,0 +1,47 @@
+# Page 2
+
+print("Hello world")
+
+# Example of a social media notification
+new_follower_notification ="You have a new follower!"
+print(new_follower_notification)
+
+# Page 3
+
+if (3>2):               # This is the header part
+    print("Hello")      # This is the body part
+else:                   # This is the header part
+    print("Bye")        # This is the body part
+
+
+# Indentation is skipped
+if (3>2):               
+print("Hello")      
+else:                   
+print("Bye")
+
+
+# Number of spaces are not same
+if (3>2):               
+       print("Hello")     
+else:                   
+ print("Bye")
+
+
+# Page 4
+
+# it's a single line comment
+print("Hello")
+
+'''
+it's a multi
+line
+comment
+
+
+'''
+
+
+# Page 5
+
+print('The sum is ' , 3+1)
