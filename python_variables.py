cbc0776070b94d6d77aba5a61587eeb2276d2c21
SprintGrad <138903886+SprintGrad@users.noreply.github.com>
2023-09-09 11:45:56 +0530

    Add files via upload

---
+++ python_variables.py
@@ -0,0 +1,21 @@
+# Page 1
+
+a = 10
+a = 20
+print(a)
+
+# Page 3
+name = input("Enter your name: ")
+print("Name :" + name)
+
+# Let us construct a calculator which performs addition
+a = int(input("Enter your first number :"))
+b = input("Enter your second number :")
+print("Here is the sum",a+b)
+
+# Page 4
+
+# Let us construct a calculator which performs addition
+a = int(input("Enter your first number :"))
+b = input("Enter your second number :")
+print("Here is the sum",a+b)
