59f3468ac91e59cf64090087b9b61cd2ee57232a
SprintGrad <138903886+SprintGrad@users.noreply.github.com>
2023-10-02 20:08:07 +0530

    Add files via upload

---
+++ data_types-numbers.py
@@ -0,0 +1,64 @@
+# Page 3
+
+x = 5
+y = 6
+print(type(x))
+print(type(y))
+
+
+
+x = 3.14
+print(type(x))
+y = -0.55
+print(type(y))
+
+
+
+#Page 4
+
+a1 = 3
+b1 = 5
+print("Here is your required sum of a1 and b1",a1+b1)
+print("Here is your required product of a1 and b1",a1*b1)
+print("Here is your required division of a1 and b1",a1/b1)
+# floor division operator
+print(b1//a1)
+'''after division it returns the largest integer
+that is less than or equal to the quotient'''
+
+
+
+
+# Page 5
+
+# Float
+a2 = 3.2
+b2 = 4.91
+print("Here is your required sum of a2 and b2", a2+b2)
+print("Here is your required product of a2 and b2", a2*b2)
+print("Here is your required division of a2 and b2", a2/b2)
+print("a2 to the power b2",a2**b2)
+print(b2//a2)
+
+
+
+
+# Page 6
+
+pi = 3.14159265359
+rounded_pi1 = round(pi, 2)
+print(rounded_pi1)
+rounded_pi2 = round(pi)
+print(rounded_pi2)
+
+
+
+x = 1+6j
+y = 3j
+print(type(x))
+print(type(y))
+
+print("Addition of x and y",x+y)
+print("Subtraction of x and y",x-y)
+print("Multiplication of x and y",x*y)
+print("Division of x and y",x/y)
